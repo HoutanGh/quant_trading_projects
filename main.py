3b381074d39c7026e53051803dbd642416260345
HoutanGh <houtan.ghaebi@gmail.com>
2024-09-27 15:31:27 +0100

    Daily Returns

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
 import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+data = yf.download('^GSPC', start='2010-01-01', end='2023-01-01')
+data['Returns'] = data['Adj Close'].pct_change().dropna()
+
+data['Returns'].plot(title=' Daily Returns of S&P 500')
+plt.show()
 
 print("Quant Project 1")
