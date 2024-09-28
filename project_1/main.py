e941793cce19f4b52995c973f9c607b9fa257dd6
HoutanGh <houtan.ghaebi@gmail.com>
2024-09-28 22:35:54 +0100

    EGARCH Model, simulating future returns based on monte carlo simulations, backtesting and calculating Value-at-Risk

--- project_1/main.py
+++ project_1/main.py
@@ -8,7 +8,7 @@ data = yf.download('^GSPC', start='2010-01-01', end='2023-01-01')
 
 data['Returns'] = data['Adj Close'].pct_change().dropna()
 
-fig, axs = plt.subplots(2, 3, figsize=(10,10))
+fig, axs = plt.subplots(3, 3, figsize=(10,10))
 
 axs[0][0].plot(data.index, data['Returns'])
 axs[0][0].set_title('Daily Returns')
@@ -23,10 +23,14 @@ print("Quant Project 1")
 
 # checking for NaN values
 print(data['Returns'].isnull().sum())
+
 from arch import arch_model
+from arch import univariate
+
 
 # define a GARCH(1, 1) model on the returns
 garch_model = arch_model(data['Returns'].dropna(), vol='Garch', p=1, q=1)
+
 
 # fit the model
 garch_result = garch_model.fit(disp='off')
@@ -86,8 +90,107 @@ import scipy.stats as stats
 stats.probplot(residuals, dist='norm', plot=plt)
 plt.title('QQ Plot of Residuals')
 plt.grid(True)
-plt.show()
+# plt.show()
+# Plot autocorrelation of residuals (ACF)
+
+
+from statsmodels.graphics.tsaplots import plot_acf
+
+# axs[1][2] = plot_acf(residuals, lags=30)
+# plt.title('ACF of Residuals')
 
 # trying out different GARCH specifications
 
 # egarch model - captures asymmetries in volatility
+
+egarch_model = arch_model(data['Returns'].dropna(), vol='EGarch', p=1, q=1)
+egarch_result = egarch_model.fit(disp="off")
+
+print(egarch_result.summary())
+
+
+# simulating future returns based on monte carlo simulations
+
+# this didn't work, so looking at how to do it manually
+# simulated_paths = garch_result.simulate(params=garch_result.params, nobs=5, method='monte carlo', repetitions=1000)
+
+omega = garch_result.params['omega']
+alpha = garch_result.params['alpha[1]']
+beta = garch_result.params['beta[1]']
+
+# print(omega, alpha, beta)
+# print(garch_result.params)
+
+n_simulations = 1000
+n_days = 5
+
+# arrays to store simuations
+
+simulated_volatility = np.zeros((n_simulations, n_days))
+simulated_returns = np.zeros((n_simulations, n_days))
+
+# starting the simulation with the last observed volatility 
+
+last_volatility = np.sqrt(garch_result.conditional_volatility.iloc[-1])
+
+# print(last_volatility)
+
+# simulating paths
+# sigma_t_array = []
+for i in range(n_simulations):
+    sigma_t = last_volatility
+    
+    for t in range(n_days):
+        shock = np.random.normal(0, 1)
+
+        # simulate next period's volatility
+        sigma_t_sqrd = omega + alpha * (shock**2) + beta * (sigma_t**2)
+        sigma_t = np.sqrt(sigma_t_sqrd)
+        # print(sigma_t)
+        simulated_volatility[i, t] = sigma_t
+
+        simulated_returns[i, t] = sigma_t * shock
+
+#         sigma_t_array.append(sigma_t)
+    
+# print(len(sigma_t_array))
+
+
+
+axs[0][2].plot(simulated_returns.T, alpha=0.01, color='blue')
+axs[0][2].set_title('Simulated Returns for 5 days')
+axs[0][2].grid(True)
+# plt.show()
+
+
+# backtesting the GARCH Model with a rolling or expanding window
+
+rolling_predictions = []
+test_size = 200
+
+for i in range(test_size):
+    train_data = data['Returns'][:-(test_size-i)]
+    garch_model = arch_model(train_data.dropna(), vol='Garch', p=1, q=1)
+    garch_result = garch_model.fit(disp='off')
+    rolling_forecast = garch_result.forecast(horizon=1)
+    rolling_predictions.append(np.sqrt(rolling_forecast.variance.values[-1, :][0]))
+
+axs[1][2].plot(range(test_size), rolling_predictions, label='Rolling Forecasted Volatility')
+axs[1][2].plot(range(test_size), data['Returns'].values[-test_size:], label='Actual Returns')
+axs[1][2].legend()
+axs[1][2].set_title('Rolling Forecast vs Actual Returns')
+axs[1][2].grid(True)
+
+# plt.show()
+
+from scipy.stats import norm
+
+# value at risk for expected loss at a given confidence over 1 day
+
+confidence_level = 0.95
+z_score = norm.ppf(confidence_level)
+forecasted_volatility = np.sqrt(forecasted_variance.iloc[-1])
+VaR_1day = z_score * forecasted_volatility
+
+print(f"1-Day VaR at 95% confidence: {VaR_1day}")
+
