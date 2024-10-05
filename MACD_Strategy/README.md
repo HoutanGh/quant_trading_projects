d2e7e9c5950ff7bc8a8e996a6f31354beaea6841
HoutanGh <houtan.ghaebi@gmail.com>
2024-10-05 22:27:56 +0100

    MACD trial file and understanding code

---
+++ MACD_Strategy/README.md
@@ -0,0 +1,4 @@
+### MACD Strategy
+
+Basic premise of trading logic:
+- when short moving average is larger than the long moving average it signals an upward trend and vice versa
--- MACD_Strategy/macd_trial.py
+++ MACD_Strategy/macd_trial.py
@@ -3,15 +3,14 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 
-# calculating the moving averages
+df = yf.download('^GSPC', start='2010-01-01', end='2023-01-01')
+
 def macd(signals):
 
     signals['ma1'] = signals['Adj Close'].rolling(window=ma1, min_periods=1, center=False).mean()
-    signals['ma1'] = signals['Adj Close'].rolling(window=ma2, min_periods=1, center=False).mean()
+    signals['ma2'] = signals['Adj Close'].rolling(window=ma2, min_periods=1, center=False).mean()
 
     return signals
 
-# signal generation
+macd(df)
 
-
-print("its working")
