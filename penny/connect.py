02628b51977259f1b10fc00e302b1b4379f7bbe1
HoutanGh <houtan.ghaebi@gmail.com>
2025-06-02 19:52:00 +0100

    trying to integrate IKBR

---
+++ penny/connect.py
@@ -0,0 +1,7 @@
+# save this as connect.py
+from ib_insync import *
+
+ib = IB()
+ib.connect('127.0.0.1', 7497, clientId=1)
+print("Connected:", ib.isConnected())
+ib.disconnect()
---
+++ penny/historical_data.py
@@ -0,0 +1,2 @@
+
+from ibapi.client import EClient
